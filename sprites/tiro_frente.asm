85be688660e876a05d8c5c82b9b686ae761a6977
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-16 20:38:30 -0300

    Update and rename tiro_baixo.asm to tiro_frente.asm

---
+++ sprites/tiro_frente.asm
@@ -0,0 +1,9 @@
+.data
+.globl sprite_tiro_baixo_dados
+
+sprite_tiro_cima_dados:
+.byte 109,109,109,109,109
+    .byte 109,109,109,109,109
+    .byte 109,109,109,109,109
+    .byte 109,109,109,109,109
+    .byte 109,109,109,109,109
