432dd10e36fe88486e198db6375579b02307406d
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-15 22:23:14 -0300

    Create print_tiro.asm

---
+++ funcoes/print_tiro.asm
@@ -0,0 +1,9 @@
+#Funcao Print_tiro
+#a0 = endereco do sprite
+#a1 = x                   
+#a2 = y                                                  
+#a3 = largura                                            
+#a4 = altura                                             
+#a5 = frame (0 ou 1)                                    
+
+
