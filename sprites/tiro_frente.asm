b9d011ff8683e48282b1b82c23f208efb7a2f5f8
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-18 21:37:34 -0300

    Delete sprites/tiro_frente.asm

--- sprites/tiro_frente.asm
+++
@@ -1,9 +0,0 @@
-.data
-.globl sprite_tiro_baixo_dados
-
-sprite_tiro_baixo_dados:
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
