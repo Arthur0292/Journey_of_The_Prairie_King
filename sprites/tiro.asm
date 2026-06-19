cbc5916f2237d9d2836ccb86c91b69506990e6eb
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-18 21:36:46 -0300

    Update and rename tiro_costas.asm to tiro.asm

--- sprites/tiro.asm
+++ sprites/tiro.asm
@@ -1,7 +1,7 @@
 .data
-.globl sprite_tiro_cima_dados
+.globl sprite_tiro_dados
 
-sprite_tiro_cima_dados:
+sprite_tiro_dados:
 .byte 182,182,182,182,182
 .byte 182,182,182,182,182
 .byte 182,182,182,182,182
