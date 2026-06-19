cd8fd20a9441844697dbd5c88c50387ed8c708bc
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-18 21:36:57 -0300

    Delete sprites/tiro_direita.asm

--- sprites/tiro_direita.asm
+++
@@ -1,9 +0,0 @@
-.data
-.globl sprite_tiro_direita_dados
-
-sprite_tiro_direita_dados:
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
-.byte 182,182,182,182,182
