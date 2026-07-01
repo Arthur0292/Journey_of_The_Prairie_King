4cc1e29d3147d1fc135610aa9d11e15408eaba58
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-07-01 19:42:52 -0300

    Update desenhar_inimigos.asm

--- funcoes/desenhar_inimigos.asm
+++ funcoes/desenhar_inimigos.asm
@@ -40,7 +40,29 @@ j proximo_desenhar
 
 mudou:
 
-addi sp, sp, -4	#salva o ra e chama o apagar
+la t5, INIMIGO_OLD_POS	#carrego o endereco da posicao antiga
+add t5, t5, s2	#somo t5 com offset
+lh a1, 0(t5)	#x
+lh a2, 2(t5)	#y
+li a3, 16	#largura e altura do inimigo
+li a4, 20
+li a5, 0	#frame 0
+
+addi sp, sp, -4	#salva o ra e chama o apagar para o frame 0
+sw ra, 0(sp)
+call Apagar
+lw ra, 0(sp)
+addi sp, sp, 4
+
+la t5, INIMIGO_OLD_POS	#carrego o endereco da posicao antiga
+add t5, t5, s2	#somo t5 com offset
+lh a1, 0(t5)	#x
+lh a2, 2(t5)	#y
+li a3, 16
+li a4, 20
+li a5, 1	#frame 1
+
+addi sp, sp, -4	#salva o ra e chama o apagar para o frame 1
 sw ra, 0(sp)
 call Apagar
 lw ra, 0(sp)
@@ -50,27 +72,45 @@ la t5, INIMIGO_POS	#muda as posicoes antigas e novas
 add t5, t5, s2
 lh t6, 0(t5)               # x atual
 lh t4, 2(t5)               # y atual
-la t5, INIMIGO_OLD_POS
-add t5, t5, s2
+la t5, INIMIGO_OLD_POS #carrego o endereco da pos antiga
+add t5, t5, s2	
 sh t6, 0(t5)
 sh t4, 2(t5)
 
+#troco as posicoes 
 la t5, INIMIGO_POS
 add t5, t5, s2
 lh a1, 0(t5)               # x
 lh a2, 2(t5)               # y
 
-la t0, inimigo_sprite	#carrego o endereco dos sprites do inimigo
+slli t3, s1, 2
+la t4, INIMIGO_DIR
+add t4, t4, t3
+lw t3, 0(t4)        # t3 = INIMIGO_DIR[i]
 li t1, 12
-mul t2, s1, t1		#calculo o offset
-add t0, t0, t2
-lw a0, 0(t0)	#guardo em t0 o offset 0
-lw a3, 4(t0)	#guardo a largura e altura
-lw a4, 8(t0)
-    
-mv a5, s3
+mul t2, t3, t1      # offset = dir * 12
 
-addi sp, sp, -4
+la t0, inimigo_sprite
+add t0, t0, t2
+lw a0, 0(t0)
+lw a3, 4(t0)
+lw a4, 8(t0)
+li a5, 0
+
+addi sp, sp, -4	#imprimo no frame 0
+sw ra, 0(sp)
+call Print
+lw ra, 0(sp)
+addi sp, sp, 4
+
+la t0, inimigo_sprite
+add t0, t0, t2
+lw a0, 0(t0)
+lw a3, 4(t0)
+lw a4, 8(t0)
+li a5, 1
+
+addi sp, sp, -4	#imprimo no frame 1
 sw ra, 0(sp)
 call Print
 lw ra, 0(sp)
