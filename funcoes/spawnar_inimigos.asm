dfca063ed7818db862ae259b86874993d423c1cc
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-07-15 17:25:25 -0300

    Update spawnar_inimigos.asm

--- funcoes/spawnar_inimigos.asm
+++ funcoes/spawnar_inimigos.asm
@@ -1,68 +1,42 @@
 spawnar_inimigos:
-addi sp, sp, -4
-sw s1, 0(sp)
-li s1, 0              # s1 = i (contador)
+	li t0, 0	#contador de spawn
+
 loop_spawn:
 
-li t1, 4	#adiciono o max de inimigos
-bge s1, t1, fim_spawn
-slli t2, s1, 2	
+	li t1, 4	#adiciono o max de inimigos
+	bge t0, t1, fim_spawn
+	slli t2, t0, 2	
 
-la t3, INIMIGO_ATIVO	#Armazeno o endereco de inimigo ativo
-add t3, t3, t2
-lw t4, 0(t3)
-bnez t4, proximo_slot      # se já está ativo pula esse slot
+	la t3, INIMIGO_ATIVO	#Armazeno o endereco de inimigo ativo
+	add t3, t3, t2
+	lw t4, 0(t3)
+	bnez t4, proximo_slot      # se jÃ¡ estÃ¡ ativo pula
 
-la t5, INIMIGO_SPAWN_POS	#Ler a posicao de spawn do inimigo
-add t5, t5, t2
-lh t6, 0(t5)	#leio a posicao x
-lh t4, 2(t5)	#leio a posicao y
+	la t5, INIMIGO_SPAWN_POS	#Ler a posicao de spawn do inimigo
+	add t5, t5, t2
+	lh t6, 0(t5)	#leio a posicao x
+	lh t4, 2(t5)	#leio a posicao y
 
-la t5, INIMIGO_POS
-add t5, t5, t2
-sh t6, 0(t5)
-sh t4, 2(t5)
+	la t5, INIMIGO_POS
+	add t5, t5, t2
+	sh t6, 0(t5)
+	sh t4, 2(t5)
 
-la t5, INIMIGO_OLD_POS          
-add t5, t5, t2
-sh t6, 0(t5)
-sh t4, 2(t5)
+	la t5, INIMIGO_OLD_POS
+	add t5, t5, t2
+	sh t6, 0(t5)
+	sh t4, 2(t5)
 
-la t5, INIMIGO_DIR           
-add t5, t5, t2
-sw zero, 0(t5)
-
-li t5, 1                   
-sw t5, 0(t3) # ativa o slot
-
-mv a1, t6  # x
-mv a2, t4  # y
-la t0, inimigo_sprite    # offset 0 = frente, que é DIR=0
-lw a0, 0(t0)	# qual frame
-lw a3, 4(t0)	#largura e altura do inimigo
-lw a4, 8(t0)
-
-
-li a5, 0	#desenha inimigo no frame 0
-addi sp, sp, -4
-sw ra, 0(sp)
-call Print
-lw ra, 0(sp)
-addi sp, sp, 4
-
-li a5, 1	#desenha inimigo no frame 1
-addi sp, sp, -4
-sw ra, 0(sp)
-call Print
-lw ra, 0(sp)
-addi sp, sp, 4
+	li t6, 1
+	sw t6, 0(t3)    # ativa o slot
 
 proximo_slot:
-addi s1, s1, 1
-j loop_spawn
+	addi t0, t0, 1
+	j loop_spawn
+
 
 fim_spawn:
-lw s1, 0(sp)
-addi sp, sp, 4
-ret
+
+	ret
+
 
